1e02c8f67503b19f12a524725309b9a056b59b81
yiyus <jgl@dyalog.com>
2025-10-31 23:10:44 +0100

    highlights

---
+++ queries/highlights.scm
@@ -0,0 +1,37 @@
+(system_command) @keyword
+
+; Identifiers
+
+(identifier) @variable
+
+(dfn_identifier) @variable.builtin
+[
+  (dop_identifier)
+  (dop1_identifier)
+  (dop2_identifier)
+] @variable.parameter.builtin
+
+
+; Literals
+
+(string_literal) @string
+(number_literal) @number
+
+; Operators
+
+(primitive) @operator
+
+[
+  "⋄"
+  ";"
+  ":"
+] @punctuation.delimiter
+
+[
+  "("
+  ")"
+  "["
+  "]"
+  "{"
+  "}"
+] @punctuation.bracket
