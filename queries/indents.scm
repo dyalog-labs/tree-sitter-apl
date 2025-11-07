ae11971a8c95755e673c07cf3299860a43e62320
yiyus <jgl@dyalog.com>
2025-11-07 18:03:32 +0100

    fix indents (working now)

--- queries/indents.scm
+++ queries/indents.scm
@@ -1,2 +1,19 @@
-[ "{" "(" "[" ] @indent
-[ "}" ")" "]" ] @dedent
+[
+  (dfn_definition)
+  (dop1_definition)
+  (dop2_definition)
+  (parenthesis)
+  (highrank)
+] @indent.begin
+(dfn_definition 
+  "}" @indent.branch)
+(dop1_definition
+  "}" @indent.branch)
+(dop2_definition
+  "}" @indent.branch)
+[
+  "}"
+  "]"
+  ")"
+] @indent.end
+(comment) @indent.auto
