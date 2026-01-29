f882ca09af237f7aaf1145759ed951e63adc3f9f
yiyus <yiyu.jgl@gmail.com>
2026-01-29 13:23:07 +0100

    locals.scm

--- queries/highlights.scm
+++ queries/highlights.scm
@@ -18,6 +18,9 @@
 ; Identifiers
 
 (identifier) @variable
+; the following line works in tree-sitter highlight,
+; but not in nvim (#is-not? predicate not supported)
+; ((identifier) @variable.builtin (#is-not? local))
 
 (dfn_identifier) @variable.builtin
 [
@@ -26,6 +29,12 @@
   (dop2_identifier)
 ] @variable.parameter.builtin
 
+(trad name: (identifier) @function)
+(trad left_arg: (identifier) @variable.builtin)
+(trad right_arg: (identifier) @variable.builtin)
+(trad left_op: (identifier) @variable.parameter.builtin)
+(trad right_op: (identifier) @variable.parameter.builtin)
+(trad local: (identifier) @variable.builtin)
 
 ; Literals
 
---
+++ queries/locals.scm
@@ -0,0 +1,9 @@
+(trad) @local.scope
+
+(trad left_arg: (identifier) @local.definition)
+(trad right_arg: (identifier) @local.definition)
+(trad left_op: (identifier) @local.definition)
+(trad right_op: (identifier) @local.definition)
+(trad local: (identifier) @local.definition)
+
+(identifier) @local.reference
